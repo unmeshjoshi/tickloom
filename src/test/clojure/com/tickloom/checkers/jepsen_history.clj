d5a9d75898ae4cacb0e34883eb319c0d9dff6b0b
Unmesh Joshi <unmesh.joshi@gmail.com>
2025-09-17 13:34:25 +0530

    Added wrapper to add jepsen history as clojure edn

--- src/test/clojure/com/tickloom/checkers/jepsen_history.clj
+++ src/test/clojure/com/tickloom/checkers/jepsen_history.clj
@@ -7,3 +7,15 @@
 (defn invoke
   [history process f value]
   (conj history {:process process :type :invoke :f f :value value}))
+
+(defn ok
+  [history process f value]
+  (conj history {:process process :type :ok :f f :value value}))
+
+(defn fail
+  [history process f value]
+  (conj history {:process process :type :fail :f f :value value}))
+
+(defn info
+  [history process f value]
+  (conj history {:process process :type :info :f f :value value}))
--- src/test/java/com/tickloom/util/JepnsenHistoryTest.java
+++ src/test/java/com/tickloom/util/JepnsenHistoryTest.java
@@ -4,7 +4,10 @@ import clojure.java.api.Clojure;
 import clojure.lang.IFn;
 import clojure.lang.IPersistentVector;
 import clojure.lang.Keyword;
+import clojure.lang.PersistentVector;
 import com.tickloom.history.Op;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
@@ -30,6 +33,9 @@ public class JepnsenHistoryTest {
         private final IFn create;
         private final IFn invoke;
         private final IFn edn_read;
+        private final IFn ok;
+        private final IFn fail;
+        private final IFn info;
 
         private IPersistentVector history;
 
@@ -37,11 +43,18 @@ public class JepnsenHistoryTest {
             return Keyword.intern(name.toLowerCase());
         }
 
+        public static IPersistentVector tuple(Object... vals) {
+            return PersistentVector.create(vals);
+        }
+
         public JepsenHistory() {
             loadNamespaces(Arrays.asList("com.tickloom.checkers.jepsen-history",
                     "clojure.edn"));
             create = Clojure.var("com.tickloom.checkers.jepsen-history", "create");
             invoke = Clojure.var("com.tickloom.checkers.jepsen-history", "invoke");
+            ok = Clojure.var("com.tickloom.checkers.jepsen-history", "ok");
+            fail = Clojure.var("com.tickloom.checkers.jepsen-history", "fail");
+            info = Clojure.var("com.tickloom.checkers.jepsen-history", "info");
             edn_read = Clojure.var("clojure.edn", "read-string");
 
             this.history = (IPersistentVector) create.invoke();
@@ -68,6 +81,24 @@ public class JepnsenHistoryTest {
             IPersistentVector expectedHistory = toEdn(ednString);
             return history.equals(expectedHistory);
         }
+
+        public JepsenHistory ok(String processId, Op op, Object value) {
+            //make history final and provide update method to keep JepsenHistory class immutable.
+            history = (IPersistentVector) ok.invoke(history, processId, createKeyword(op.name()), value);
+            return this;
+        }
+
+        public JepsenHistory fail(String processId, Op op, Object value) {
+            //make history final and provide update method to keep JepsenHistory class immutable.
+            history = (IPersistentVector) fail.invoke(history, processId, createKeyword(op.name()), value);
+            return this;
+        }
+
+        public JepsenHistory info(String processId, Op op, Object value) {
+            //make history final and provide update method to keep JepsenHistory class immutable.
+            history = (IPersistentVector) info.invoke(history, processId, createKeyword(op.name()), value);
+            return this;
+        }
     }
 
     @Test
@@ -78,5 +109,52 @@ public class JepnsenHistoryTest {
         history.invoke("client-1", Op.WRITE, "value");
 
         assertTrue(history.matches("[{:process \"client-1\" :type :invoke :f :write :value \"value\"}]"));
+    }
+
+    @Test
+    public void testInvokeEventWithKVHistoryValue() {
+        JepsenHistory history = new JepsenHistory();
+
+        //process, type, key, value
+        history.invoke("client-1", Op.WRITE, JepsenHistory.tuple("key", "value"));
+
+        assertTrue(history.matches("[{:process \"client-1\" :type :invoke :f :write :value [\"key\" \"value\"]}]"));
+    }
+
+    @Test
+    public void testOkEvent() {
+        JepsenHistory history = new JepsenHistory();
+        history.ok("client-1", Op.WRITE, JepsenHistory.tuple("key", "value"));
+        assertTrue(history.matches("[{:process \"client-1\" :type :ok :f :write :value [\"key\" \"value\"]}]"));
+    }
+
+    @Test
+    public void testFailEvent() {
+        JepsenHistory history = new JepsenHistory();
+        history.fail("client-1", Op.WRITE, JepsenHistory.tuple("key", "value"));
+        assertTrue(history.matches("[{:process \"client-1\" :type :fail :f :write :value [\"key\" \"value\"]}]"));
+    }
+
+    @Test
+    public void testInfoEvent() {
+        JepsenHistory history = new JepsenHistory();
+        history.info("client-1", Op.WRITE, JepsenHistory.tuple("key", "value"));
+        assertTrue(history.matches("[{:process \"client-1\" :type :info :f :write :value [\"key\" \"value\"]}]"));
+    }
+
+    @Test
+    public void testHistoryWithMultipleEvents() {
+        JepsenHistory history = new JepsenHistory();
+        history.invoke("client-1", Op.WRITE, JepsenHistory.tuple("key", "value"))
+                .info("client-1", Op.WRITE, JepsenHistory.tuple("key", "value"))
+                .invoke("client-2", Op.WRITE, JepsenHistory.tuple("key2", "value2"))
+                .ok("client-2", Op.WRITE, JepsenHistory.tuple("key2", "value2"));
+
+        assertTrue(history.matches("[" +
+                "{:process \"client-1\" :type :invoke :f :write :value [\"key\" \"value\"]}" +
+                "{:process \"client-1\" :type :info :f :write :value [\"key\" \"value\"]}" +
+                "{:process \"client-2\" :type :invoke :f :write :value [\"key2\" \"value2\"]}" +
+                "{:process \"client-2\" :type :ok :f :write :value [\"key2\" \"value2\"]}" +
+                "]"));
     }
 }
